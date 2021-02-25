dbe4af2f25874f8a6ce5bbb99946570eea743de4
hovanduong <52810802+hovanduong@users.noreply.github.com>
2021-02-25 22:05:55 +0700

    Rin day code len git ne

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: WebView(initialUrl: "https://tiki.vn",
+        javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
